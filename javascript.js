05eb281c6b4aa30e37cbac75cb76381711e7573e
dk99kodehode <dk99.kodehode@gmail.com>
2026-06-04 12:20:15 +0200

    added map & filter to electronics oppgave 3

--- javascript.js
+++ javascript.js
@@ -10,28 +10,28 @@ const products = [
   { name: "Sneakers", price: 300, category: "clothing" },
 ];
 
-/* Oppgave 1* Bruker FILTER for å sjekke om item pricen er mindre enn cash */
+/*Oppgave 1* Bruker FILTER for å sjekke om item pricen er mindre enn cash */
 const cash = 200;
 const affordableItems = products.filter((item) => item.price <= cash);
 console.log(`Med 200kr kan eg kjøpe disse`, affordableItems);
 
-/* Oppgave 2* Bruker MAP for å lage et nytt array  productNames   */
+/*Oppgave 2* Bruker MAP for å lage et nytt array av products   */
 const productNames = products.map((product) => `${product.name}`);
 console.log(`Navnet på produktene er `, productNames);
 
-/* Oppgave 3*  Bruker Filter for category for å sjekke hver objcet i  array om categorien inneholder "electronics"*/
+/*Oppgave 3*  Bruker filter for å filtrere ut product som har kategorien "Electronics" der etter gi meg alle produkt navne som har kategorien "Electronic" med map*/
 
-const categoryType = products.filter(
-  (product) => product.category === "electronics",
-);
+const electronicsProducts = products
+  .filter((product) => product.category === "electronics")
+  .map((product) => `${product.name} har  kategorien electronics`);
 
-console.log(`Disse har categorien Electronics`, categoryType);
+console.log(electronicsProducts);
 
-/* Oppgave 4* Bruker SOME for å ha en boolean for product price koster 1000 eller meir */
+/*Oppgave 4* Bruker SOME / Boolean for å sjekke om prisen er 1000 eller meir */
 const aboveTresh = products.some((product) => product.price >= 1000);
 console.log(aboveTresh);
 
-/* Oppgave 5* Bruker REDUCE for å combinere 1 og 2 så 3,4  heilt til arrayen er tom og gir meg en total sum av pricen*/
+/*Oppgave 5* Bruker REDUCE for å combinere 1 og 2 så 3,4  heilt til arrayen er tom og gir meg en total sum av pricen*/
 const priceTotal = products.reduce(
   (acc, currenValue) => acc + currenValue.price,
   0,
